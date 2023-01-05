562ca6b9d2fcb4b44deeaf7df6e186e89418832f
Zeeshan Hyder <109053785+Zeeshann1@users.noreply.github.com>
2023-01-05 14:31:40 +0800

    Update class.cpp

--- classes_and_objects/class.cpp
+++ classes_and_objects/class.cpp
@@ -23,17 +23,26 @@ class Room {
 
 int main() {
 
-    // create object of Room class
-    Room room1;
+   // create object of Room class
+    Room room1, room2;
 
     // assign values to data members
-    room1.length = 42.5;
-    room1.breadth = 30.8;
-    room1.height = 19.2;
+    room1.length = 34;
+    room1.breadth = 28;
+    room1.height = 20;
+
+    room2.length = 42.5;
+    room2.breadth = 30.8;
+    room2.height = 19.2;
+
 
     // calculate and display the area and volume of the room
-    cout << "Area of Room =  " << room1.calculateArea() << endl;
-    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+    cout << "Area of Room1 =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room1 =  " << room1.calculateVolume() << endl;
+
+
+    cout << "Area of Room2 =  " << room2.calculateArea() << endl;
+    cout << "Volume of Room2 =  " << room2.calculateVolume() << endl;
 
     return 0;
 }
